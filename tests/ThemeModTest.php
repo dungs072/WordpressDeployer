2ed42f8646ed9af83d6a8c6477c5a79b724c38fa
dungs072 <82323846+dungs072@users.noreply.github.com>
2024-06-07 01:26:19 +0700

    fix

--- tests/ThemeModTest.php
+++ tests/ThemeModTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
-use Blocksy\Database;
+include('wp-content/themes/blocksy/database.php');
 final class ThemeModTest extends TestCase
 {
     private $themeMods;
