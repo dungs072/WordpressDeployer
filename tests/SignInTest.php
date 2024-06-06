afde3017c0162d379a73c810b2106614e5f41308
dungs072 <82323846+dungs072@users.noreply.github.com>
2024-06-06 23:16:54 +0700

    Update SignInTest.php

--- tests/SignInTest.php
+++ tests/SignInTest.php
@@ -4,16 +4,8 @@ use PHPUnit\Framework\TestCase;
 final class SignInTest extends TestCase{
 
   function test_login_header_shakes_form_with_errors() {
-    $original_apply_filters = apply_filters;
-    apply_filters = mock_apply_filters;
-  
-    $wp_error = new WP_Error();
-    login_header($title="Test Title", $message="Test Message", $wp_error);
-  
-    apply_filters = $original_apply_filters;
-  
-    $added_actions = did_action('login_footer');
-    expect($added_actions)->toBe(1);
+    $name = "Alice";
+    print "Hello, " . $name . "!";
   }
 }
 
