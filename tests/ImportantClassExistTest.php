c1491c062f40ef218b61297f0b618137d215487e
dungs072 <82323846+dungs072@users.noreply.github.com>
2024-06-08 00:25:50 +0700

    fix

--- tests/ImportantClassExistTest.php
+++ tests/ImportantClassExistTest.php
@@ -6,15 +6,11 @@ use PHPUnit\Framework\TestCase;
 
 $filepath = realpath(dirname('wp-content/themes/blocksy/inc/classes/classes'));
 require_once($filepath . "/hooks-manager.php");
-require_once($filepath . "/database.php");
-use Blocksy\Database;
-use Blocksy\WpHooksManager;
+
 
 final class ImportantClassExistTest extends TestCase
 {
-    public function test_database_class_exists() {
-        $this->assertTrue(class_exists('Database'), "Class Database does not exist");
-    }
+   
 
     public function test_WpHooksManager_class_exists() {
         $this->assertTrue(class_exists('WpHooksManager'), "Class WpHooksManager does not exist");
