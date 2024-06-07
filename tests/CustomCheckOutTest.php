7fd9dc344440d574cf204ae2ba0c21df6ea0d2cf
dungs072 <82323846+dungs072@users.noreply.github.com>
2024-06-07 11:09:18 +0700

    change test case

--- tests/CustomCheckOutTest.php
+++ tests/CustomCheckOutTest.php
@@ -14,7 +14,6 @@ final class CustomCheckOutTest extends TestCase
 
     public function testHasCustomCheckout()
     {
-        // Mock the global $post variable
         $GLOBALS['post'] = null;
 
         // Scenario 1: No custom checkout plugins or Cartflows step
@@ -22,14 +21,14 @@ final class CustomCheckOutTest extends TestCase
         $actualResult = $this->has_custom_checkout();
         $this->assertEquals($expectedResult, $actualResult, "Should return true without custom checkout plugins or Cartflows step");
 
-        // Scenario 2: FluidCheckout exists
-        class_exists('FluidCheckout', true); // Mock the existence of FluidCheckout class
-        $expectedResult = false;
-        $actualResult = $this->has_custom_checkout();
-        $this->assertEquals($expectedResult, $actualResult, "Should return false with FluidCheckout class");
+        // // Scenario 2: FluidCheckout exists
+        // class_exists('FluidCheckout', true); // Mock the existence of FluidCheckout class
+        // $expectedResult = false;
+        // $actualResult = $this->has_custom_checkout();
+        // $this->assertEquals($expectedResult, $actualResult, "Should return false with FluidCheckout class");
 
-        // Reset the mock
-        class_exists('FluidCheckout', false);
+        // // Reset the mock
+        // class_exists('FluidCheckout', false);
 
         // Scenario 3: WFFN_Core exists
         class_exists('WFFN_Core', true); // Mock the existence of WFFN_Core class
@@ -59,7 +58,7 @@ final class CustomCheckOutTest extends TestCase
         $this->assertEquals($expectedResult, $actualResult, "Should respect filter override");
 
         // Remove the filter
-        //remove_filter('blocksy:woocommerce:checkout:has-custom-markup');
+        remove_filter('blocksy:woocommerce:checkout:has-custom-markup');
     }
 
     private function has_custom_checkout()
